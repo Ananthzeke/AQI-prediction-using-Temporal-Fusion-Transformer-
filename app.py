90d5845479d40d671a8d7660ba0d81ac9d5be60e
ANANTH ZEKE <anandh50sankar@gmail.com>
2023-03-05 09:24:16 +0530

    change in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import time
 import calendar
 # Load the data
-df = pd.read_csv('/home/ananth/slit/aqi_2016_to_2023.csv')
+df = pd.read_csv('aqi_2016_to_2023.csv')
 
 # Convert date column to datetime
 df['date'] = pd.to_datetime(df['date'])
